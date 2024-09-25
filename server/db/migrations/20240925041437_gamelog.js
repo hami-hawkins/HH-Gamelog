e6e343287677a75366cf99f06baa82abf5eed5ef
Hami Hawkins <hhaami@gmail.com>
2024-09-25 17:55:41 +1200

    Update seeds

--- server/db/migrations/20240925041437_gamelog.js
+++ server/db/migrations/20240925041437_gamelog.js
@@ -6,10 +6,20 @@ export async function up(knex) {
   return knex.schema.createTable('gamelog', (table) => {
     table.increments('id').primary()
     table.string('game')
+    table.boolean('played_before')
+    table.boolean('clocked_before')
     table.string('platform')
-    table.string('release_date')
-    table.boolean('owned')
-    table.boolean('clocked')
+    table.string('start_date')
+    table.string('finish_date')
+    table.string('playtime_estimate')
+    table.string('playtime_final')
+    table.integer('gameplay_rating')
+    table.integer('story_rating')
+    table.integer('graphics_rating')
+    table.integer('performance_rating')
+    table.integer('fun_rating')
+    table.integer('final_rating')
+    table.string('final_thoughts')
   })
 }
 
